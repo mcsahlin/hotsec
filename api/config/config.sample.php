30c023ed56e23f9c4c5a707f240f81d9160ad46f
mcsahlin <mcsahlin@outlook.com>
2023-09-05 10:47:32 +0200

    added README, sample.config file and minor fixes

---
+++ api/config/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'db' => [
+    'host' => 'localhost',
+    'username' => '',
+    'password' => '',
+    'dbname' => '',
+    'charset' => 'utf8mb4',
+  ],
+  'base_url' => 'http://example/server/',
+];
